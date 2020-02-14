73ee2f02e732ef5c20079b44c6015f2826ee38fa
tboychuk <tarass.boychuk@gmail.com>
2020-02-14 23:36:33 +0200

    * remove all completed examples

--- spring-boot-jpa-examples/src/main/java/ua/org/javaday/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.org.javaday.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.org.javaday.entity.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
